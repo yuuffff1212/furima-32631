9b2694fb6823dfd97037ab6be850415620863733
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-01-27 18:29:37 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,23 +1,22 @@
 class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  bolongs_to :user
-  belongs_to :shipping_cost_id
-  belongs_to :shipping_date_id
-  belongs_to :shipping_region_id
+  belongs_to :user
+  belongs_to :shipping_cost
+  belongs_to :shipping_date
+  belongs_to :shipping_region
   belongs_to :item_category
-  belongs_to :item_state_id
+  belongs_to :item_state
 
   with_options presence: true do
     validates :name
     validates :text
-    validates :text
-    validates :price format: {with: /\A[0-9]+\z/, message: " is invalid. Input half-width alphanumeric characters."}
+    validates :price, numericality: { message: " is invalid. Input half-width alphanumeric characters."}
     validates :shipping_region_id 
     validates :shipping_date_id
     validates :shipping_cost_id
     validates :item_state_id
-    validates :item_category
+    validates :item_category_id
     validates :image
   end
 
@@ -25,7 +24,7 @@ class Item < ApplicationRecord
   with_options numericality: { other_than: 1 } do
     validates :shipping_date_id
     validates :shipping_cost_id
-    validates :item_category
+    validates :item_category_id
     validates :item_state_id
   end
 end
