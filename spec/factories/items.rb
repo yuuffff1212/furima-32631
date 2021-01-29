52941a04e2fc11cfeda0be6e1cf2d335d93b214b
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-01-29 21:34:12 +0900

    ハッシュの値の修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,13 +2,12 @@ FactoryBot.define do
   factory :item do
     name {"商品"}
     text {"商品の説明"}
-    #price {Faker::Price.between(from: '300', to: '9999999')}
     price {300}
     shipping_region_id {1}
-    shipping_date_id {2}
-    shipping_cost_id {2}
-    item_category_id {2}
-    item_state_id {2}
+    shipping_date_id {1}
+    shipping_cost_id {1}
+    item_category_id {1}
+    item_state_id {1}
     association :user
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
