b6f62f774f3c6b72a4dfd4fef324183a3834863d
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-01-30 21:42:19 +0900

    Revert "商品編集機能の実装"
    This reverts commit 39f342d3be9de016d6c8d795ac0ab95296310c8c.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,20 +23,6 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    #item = Item.update(item_params) 
-   if @item.update(item_params)
-    redirect_to item_path
-   else
-    render :edit
-   end
-  end
-
   private
   
   def item_params
