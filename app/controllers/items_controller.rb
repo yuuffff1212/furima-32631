b26d5f71bc12e16316fb3577ab20a9d161db86cd
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-01-30 22:37:49 +0900

    投稿者でないとトップページに遷移する処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
 
   def index
@@ -20,15 +21,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    unless current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 
   def update
-    @item = Item.find(params[:id])
    if @item.update(item_params)
     redirect_to item_path
    else
@@ -40,5 +41,9 @@ class ItemsController < ApplicationController
   
   def item_params
     params.require(:item).permit(:image, :name, :text, :price, :shipping_cost_id, :shipping_date_id, :shipping_region_id, :item_category_id, :item_state_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
