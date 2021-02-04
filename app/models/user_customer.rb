0afbe8c0a41a9b778923f7d326cfcf2147c19d3c
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-02-04 15:08:56 +0900

    バリデーションの修正

--- app/models/user_customer.rb
+++ app/models/user_customer.rb
@@ -7,15 +7,15 @@ class UserCustomer
     validates :shipping_region_id, numericality: { other_than: 0 } 
     validates :city
     validates :house_number
-    validates :tell, length: { maximum: 11 }
+    validates :tell, format: { with: /\A[0-9]{11}\z/, message: "only input number.Tell is too long (maximum is 11 characters) " }
     validates :user_id
     validates :item_id
     validates :token
   end
-  validates :tell, numericality: {message: "Input only number"}
+  # validates :tell
 
   def save
-    customer = Customer.create!(user_id: user_id, item_id: item_id)
-    Address.create!(postal_code: postal_code, city: city, house_number: house_number, building_number: building_number, tell: tell, shipping_region_id: shipping_region_id, customer_id: customer.id)
+    customer = Customer.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, city: city, house_number: house_number, building_number: building_number, tell: tell, shipping_region_id: shipping_region_id, customer_id: customer.id)
   end
 end
